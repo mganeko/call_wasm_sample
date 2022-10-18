87dc8d2d4e059188dde38271c84820890dad9736
mganeko <masashi.g@gmail.com>
2022-10-18 21:13:45 +0900

    async simple

--- call_wasm_async.mjs
+++ call_wasm_async.mjs
@@ -10,27 +10,24 @@
 import fs from "fs";
 const content = fs.readFileSync("./func.wasm");
 
+console.warn("-- start compiling wasm --")
 
-async function callWasm(content) {
-  const module = await WebAssembly.compile(content)
-  .catch((e) => {
-    console.error("ERROR:", e);
-  });
-  console.warn("-- wasm compiled --");
+const module = await WebAssembly.compile(content)
+.catch((e) => {
+  console.error("ERROR:", e);
+});
+console.warn("-- wasm compiled --");
 
-  const lib = new WebAssembly.Instance(module, {
-    env: {},
-  }).exports;
-  console.warn("-- wasm instance ready. lib:", lib);
+const lib = new WebAssembly.Instance(module, {
+  env: {},
+}).exports;
+console.warn("-- wasm instance ready. lib:", lib);
 
-  // call func
-  console.warn("== Result of wasm func ==");
-  const ret = lib.func();
-  console.log(ret); // 42
-}
+// call func
+console.warn("== Result of wasm func ==");
+const ret = lib.func();
+console.log(ret); // 42
 
-console.warn("-- start loading wasm --")
-await callWasm(content);
 
 console.warn("--- end ---");
 
